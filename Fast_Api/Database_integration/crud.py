fafe77473459b02962e7dd465a536ca26b00338f
Maaz Amir <amirmaaz16@gmail.com>
2026-09-05 14:59:57 +0530

    Add CRUD operations for Todo items in FastAPI

--- Fast_Api/Database_integration/crud.py
+++ Fast_Api/Database_integration/crud.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine,Column,Integer,String
 from sqlalchemy.orm import sessionmaker, declarative_base,Session
-from fastapi import FastAPI,Depends
+from fastapi import FastAPI,Depends,HTTPException
 from typing import Annotated
 
 
@@ -42,5 +42,67 @@ def create_todo(title:str,db:Annotated[Session,Depends(get_db)]):
     db.refresh(todo)
     return{
         'message':'Todo is successfully created.',
+        'data':todo
+    }
+    
+#Read all data
+@app.get('/todos')
+def get_todos(db:Annotated[Session,Depends(get_db)]):
+    todos = db.query(Todo).all()
+    return {
+        'Total':len(todos),
+        'data':todos
+    }
+    
+#fetching data according to id
+@app.get('/todos/{todo_id}')
+def get_todo(todo_id:int,db:Annotated[Session,Depends(get_db)]):
+    todo = db.query(Todo).filter(Todo.id == todo_id).first()
+    
+    if not todo:
+        raise HTTPException(
+            status_code=404,
+            detail='Todo not found!'
+        )
+    return todo
+
+#Update
+@app.put('/todos/{todo_id}')
+def update_todo(todo_id:int,title:str,db:Annotated[Session,Depends(get_db)],completed:bool):
+    todo = db.query(Todo).filter(Todo.id==todo_id).first()
+    
+    if not todo:
+        raise HTTPException(
+            status_code=404,
+            detail='Todo not found!'
+        )
+        
+    todo.title = title
+    todo.completed = completed
+    
+    db.commit()
+    db.refresh(todo)
+    
+    return {
+    'message':f'Todo:{todo_id} updated successfully.',
+    'data':todo        
+    }
+    
+@app.delete('/todos/{todo_id}')
+def update_todo(todo_id:int,db:Annotated[Session,Depends(get_db)]):
+    todo = db.query(Todo).filter(Todo.id==todo_id).first()
+    
+    if not todo:
+        raise HTTPException(
+            status_code=404,
+            detail='Todo not found!'
+        )
+    
+    db.delete(todo)
+    db.commit()
+    # db.refresh(todo)
+    
+    return{
+        'message':f'Todo of id={todo_id} is deleted successfully.',
         'data':todo
     }
